805f89f396f6982e011c48f16bbd761e9ea79a3f
Gabriele Falace <gabrielefalace@gmail.com>
2021-02-27 08:24:07 +0100

    fix: add forgottten SecurityConfig file

---
+++ src/main/kotlin/com/falace/auth/SecurityConfig.kt
@@ -0,0 +1,21 @@
+package com.falace.auth
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/login").allowedOrigins("*")
+            }
+        }
+    }
+}
