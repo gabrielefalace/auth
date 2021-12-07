ac29ae81bfed4dcdc526471329376e42ab1739b9
Gabriele Falace <gabrielefalace@gmail.com>
2021-12-07 14:14:47 +0100

    fix: RestTemplate instead of HttpClient

--- src/main/kotlin/com/falace/auth/social/FacebookConnectController.kt
+++ src/main/kotlin/com/falace/auth/social/FacebookConnectController.kt
@@ -1,11 +1,12 @@
 package com.falace.auth.social
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 
 
 @CrossOrigin(origins = ["*"], maxAge = 3600)
@@ -21,11 +22,12 @@ class FacebookConnectController {
         @RequestParam email: String
     ) {
 
-        val client = HttpClient.newHttpClient()
-        val request =
-            HttpRequest.newBuilder(URI("https://graph.facebook.com/debug_token?input_token=$userAccessToken&access_token=$appToken"))
-        val response = client.send(request.build(), HttpResponse.BodyHandlers.ofString())
-        if (response.statusCode() !in 200 until 300)
+        val response = RestTemplate().getForEntity(
+            URI("https://graph.facebook.com/debug_token?input_token=$userAccessToken&access_token=$appToken"),
+            Any::class.java
+        )
+
+        if (response.statusCode.value() !in 200 until 300)
             throw IllegalAccessException("Facebook login not successful!")
     }
 
